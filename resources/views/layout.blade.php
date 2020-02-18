777c055a793e8cd68f3703888e6fc377a8921551
Gwenaelle Cadic <gcadic@ensc.fr>
2020-02-18 17:22:19 +0000

    (CRLF remplace LF pour info et la navbar a un semblant d'existence

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="../style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
@@ -21,10 +21,10 @@
     <nav class="navbar navbar-expand-sm" style="background-color: #994f17;">
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="#">Randonnées</a>
+      <a class="nav-link" href="randonnees.blade.php">Randonnées</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Nouvelle randonnée</a>
+      <a class="nav-link" href="randonnees.blade.php">Nouvelle randonnée</a>
     </li>
   </ul>
 
